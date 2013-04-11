975c96f40daaafbfe4dcd2422cbdd3b614ae1ece
Max Clements <max@clements.za.net>
2013-04-11 21:51:33 +0100

    Changed Context

--- vm.sh
+++ vm.sh
@@ -2,15 +2,15 @@
 #note use of '-' in '-EOF1' - Escapes tab at beginning of lines
 # Script to call MWI extentions in CallManager to turn MWI on or off.
 
-if [[ $3 -gt 0 ]] then
+if [[ $3 -gt 0 ]]; then
 
 CALLFILE=$(cat <<-EOF1
-Channel: H323/1000$2
+Channel: local/1000$2@ccm-mwi
 MaxRetries: 1
 RetryTime: 120
 WaitTime: 45
 Context: ccm-mwi
-Extension: wait
+Extension: 1000$2
 Priority: 1
 
 EOF1)
@@ -20,12 +20,12 @@ echo "$CALLFILE" >> "/var/spool/asterisk/outgoing"/$(date +%Y%mNaVI%M%S)-$1
 else
 
 CALLFILE=$(cat <<-EOF1
-Channel: H323/1001$2
+Channel: local/1001$2@ccm-mwi
 MaxRetries: 1
 RetryTime: 120
 WaitTime: 45
 Context: ccm-mwi
-Extension: wait
+Extension: 1001$2
 Priority: 1
 
 EOF1)
