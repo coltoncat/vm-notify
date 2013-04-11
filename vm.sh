d8a4657f7cab6b9c86d8db49f9472f1cdb4a3bfd
Max Clements <max@clements.za.net>
2013-04-11 20:47:30 +0100

    Updated for bash

--- vm.sh
+++ vm.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 #note use of '-' in '-EOF1' - Escapes tab at beginning of lines
 # Script to call MWI extentions in CallManager to turn MWI on or off.
 
-if [ $3 -gt 0 ]; then
+if [[ $3 -gt 0 ]] then
 
-	CALLFILE=$(cat <<-EOF1
+CALLFILE=$(cat <<-EOF1
 Channel: Local/1000$2
 MaxRetries: 1
 # Retry in 2 min
@@ -17,11 +17,11 @@ Priority: 1
 
 EOF1)
 
-	echo "$CALLFILE" >> "/var/spool/asterisk/outgoing"/$(date +%Y%mNaVI%M%S)-$1
+echo "$CALLFILE" >> "/var/spool/asterisk/outgoing"/$(date +%Y%mNaVI%M%S)-$1
 
 else
 
-	CALLFILE=$(cat <<-EOF1
+CALLFILE=$(cat <<-EOF1
 Channel: Local/1001$2
 MaxRetries: 1
 # Retry in 2 min
@@ -34,6 +34,6 @@ Priority: 1
 
 EOF1)
 
-	echo "$CALLFILE" >> "/var/spool/asterisk/outgoing"/$(date +%Y%mNaVI%M%S)-$1
+echo "$CALLFILE" >> "/var/spool/asterisk/outgoing"/$(date +%Y%mNaVI%M%S)-$1
 
 fi
